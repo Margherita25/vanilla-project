05b733dcc6913fb26885247d6e0c35ab4a8ca283
Margherita25 <85751121+Margherita25@users.noreply.github.com>
2021-07-22 16:47:16 +0000

    Added Search Engine

--- src/app.js
+++ src/app.js
@@ -42,8 +42,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].descriptions);
 }
 
-let apiKey = "0df4dea8166c0fac0b7799785e3aa71b";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "0df4dea8166c0fac0b7799785e3aa71b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
